71407d2386c072e9bdbdc5fc0ec7d238741bf06f
Victor Bjelkholm <victor@typeform.com>
2015-10-25 02:04:23 +0200

    Component for pinning content

--- frontend/src/views/PinView.js
+++ frontend/src/views/PinView.js
@@ -1,6 +1,30 @@
 import React from 'react'
 
+import history from '../history'
+
 class PinView extends React.Component {
+	constructor(props) {
+		super(props)
+		if(window.location.pathname === '/') {
+			this.props.history.replaceState(null, '/pins')
+		}
+		this.state = {
+			hash: null
+		}
+	}
+	handleOnChange(ev) {
+		this.setState({
+			hash: ev.target.value.trim()
+		})
+	}
+	handleOnClick() {
+		const hash = this.state.hash
+		this.setState({hash: null})
+		fetch('http://localhost:3001/api/pin/' + hash, {method: 'POST'})
+		.then(() => {
+			this.props.history.replaceState(null, '/pins/'+hash)
+		})
+	}
 	render() {
 		return <div className="wrapper">
 			<div>
@@ -9,10 +33,10 @@ class PinView extends React.Component {
 			<div className="content">
 				<div className="form">
 					<label htmlFor="hash_input">Hash:</label>
-					<input id="hash_input" name="hash_input" type="text"></input>
+					<input id="hash_input" name="hash_input" type="text" value={this.state.hash} onChange={this.handleOnChange.bind(this)}></input>
 					<div className="clearfix"></div>
 				</div>
-				<button>Pin it forever</button>
+				<button onClick={this.handleOnClick.bind(this)}>Pin it forever</button>
 			</div>
 			<div className="notes">
 				<h3>Developer?</h3>
